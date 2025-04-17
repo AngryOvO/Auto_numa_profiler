043b75fdef3b2d57f7edfe85d4cea3dc87f8cbc2
AngryOvO <101005580+AngryOvO@users.noreply.github.com>
2025-04-17 16:44:43 +0000

    replay

--- linux-6.8/mm/migrate.c
+++ linux-6.8/mm/migrate.c
@@ -2732,7 +2732,7 @@ static int numa_folio_stats_show(struct seq_file *m, void *v)
 
             for (unsigned long pfn = start_pfn; pfn < end_pfn; pfn++) {
                 unsigned long node_pfn = get_pfn_for_node(nid, pfn);
-                if (atomic_read(&stat[node_pfn].current_migrate_count) != 1) {
+                if (atomic_read(&stat[node_pfn].current_migrate_count) > 0) {
                     seq_printf(m, "folio node: %d, pfn: %lu, source_nid: %d, migrate_count: %d\n",
                                nid, pfn, stat[node_pfn].source_nid,
                                atomic_read(&stat[node_pfn].current_migrate_count));
